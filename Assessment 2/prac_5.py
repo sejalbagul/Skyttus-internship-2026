2edc80135cb172699e2e55fe0b9da0acbbcfde9d
sejalbagul <sejalbagul27@gmail.com>
2026-01-14 21:11:38 +0530

    Add equality check for two user-input numbers

---
+++ Assessment 2/prac_5.py
@@ -0,0 +1,9 @@
+# TAKE TWO NUMBERS 
+a=int(input("Enter first number:"))
+b=int(input("Enter second number :"))
+
+#CHECK EQUALITY
+if a==b:
+    print("Both numbers are equal")
+else:
+    print("Both numbers are not equal")
