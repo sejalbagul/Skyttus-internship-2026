ad39c0caacf8f6b19bdb2e70a5edfdd5dccad2a6
sejalbagul <sejalbagul27@gmail.com>
2026-01-15 00:09:48 +0530

    Add function to calculate area of rectangle

---
+++ Assessment 4/python function/area_react.py
@@ -0,0 +1,9 @@
+# FUNCTION TO FIND AREA OF RECTANGLE 
+
+def area_rectangle(length,width):
+    return length * width
+
+l = float(input("Enter length: "))
+w = float(input("Enter width: "))
+
+print(area_rectangle(l,w))
