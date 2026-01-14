c42f10e6db73d76cf969317e119ace5771d037dd
sejalbagul <sejalbagul27@gmail.com>
2026-01-14 21:13:18 +0530

    Create prac_8.py

---
+++ Assessment 2/prac_8.py
@@ -0,0 +1,11 @@
+# TAKE NUMBER AS STRING 
+num = input("Enter a number: ")
+
+#CONVERT  STRING TO INTEGER
+num_int =int(num)
+
+#MULTIPLY BY 10
+result=num_int*10
+
+#print result
+print("Result:",result)
