c7a389835fedc921cefc01216ae13bd382955f99
sejalbagul <sejalbagul27@gmail.com>
2026-02-19 19:46:30 +0530

    Add files via upload

---
+++ ASP.NET /phase-1/assessment-5/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EduManage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
